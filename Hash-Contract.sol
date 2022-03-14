90268481e4e0cccc559d4ffd97bc4e2a60667419
jabedongit <35280223+jabedongit@users.noreply.github.com>
2022-03-14 23:29:45 +1100

    Update Hash-Contract.sol

--- Hash-Contract.sol
+++ Hash-Contract.sol
@@ -1,34 +1,63 @@
 pragma solidity >=0.6.22 <0.8.0;
 
-contract Hash-Contract {
+contract DSContract {
 
-function addHash(string data, string metaData) public onlyPatient
-{ 
-  if (data.isValue) 
-  {
-       bytes memory _bData = bytes(data);
-       bytes memory _bMetaData = bytes(metaData);
-       string memory storeData = new string(_bdata.length + _bmetadata.length);
-      
-       registry[hash]= sha256(abi.encodePacked(storeData));
-    }
-   else
-         revert()
-}
-   
-   
-function verifyHash(string queryData, string metaData) public view returns(bool)
-{ 
-    bytes memory _bData = bytes(queryData);
-   bytes memory _bMetaData = bytes(metaData);
-   string memory storeData = new string(_bdata.length + _bmetadata.length);
-       
-    if(registry[hash]= sha256(abi.encodePacked(storeData)))
-    {
-       return true;
-  }
-  else
-       return false;
+address payable public patient;
+mapping (address => mapping (string => subjectsAccessRights)) accessList;
+
+struct subjectsAccessRights { //The access rights for data users. It is created per each operation
+string operation; //like read, write
+string permission; //like allowed, denied
+uint grantTime; //the time that the permission is granted to the user
+bool isValue; //for duplicate check
  }
+
+function addConsumer(address _consumer, string memory _operation, string memory _permission, uint _grantTime) public 
+{ 
+    if (accessList[_consumer][_operation].isValue) 
+        revert(); //the data consumer and right already exists
+    else { 
+        accessList[_consumer][_operation].operation = _operation; 
+        accessList[_consumer][_operation].permission = _permission; 
+        accessList[_consumer][_operation].grantTime = _grantTime;
+        } 
+} 
+
+
+function deleteConsumer(address _consumer, string memory _operation ) public 
+{ 
+    if(accessList[_consumer][_operation].isValue) 
+        delete accessList[_consumer][_operation];
+    else 
+        revert(); //if the data consumer doesn't exist, revert 
+} 
+
+
+function getAccessRight(address _consumer, string memory _operation) public view returns (string memory _permission)
+{ 
+    if (accessList[_consumer][_operation].isValue)
+        {
+        _permission = accessList[_consumer][_operation].permission;
+        } 
+    else 
+        revert(); //if the data consumer does not exist, revert. 
+ return _permission;
+}  
+
+function updateAccessRights(address _consumer, string memory _operation,string memory _permission) public  
+{ 
+    if (accessList[_consumer][_operation].isValue)
+    { 
+         accessList[_consumer][_operation].permission = _permission; 
+    } 
+    else
+        revert(); //if the data consumer doesn't exist, revert 
+}
+
+ function deleteDS() public 
+ { 
+    selfdestruct(patient); 
+ }
+ 
 
 }
